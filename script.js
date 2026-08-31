9b12420880a7aa3c27cb973c7e1313e2b383572c
talitabarbosarodrigues-coder <talita.barbosa.rodrigues@escola.pr.gov.br>
2026-08-31 14:26:24 -0300

    ttttt

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ botoes.forEach(function (botao) {
 let curtiu = false ;
 botao.addEventListener("click", botaoClicado);
 function botaoClicado () {
-let texto = botao.querySelectorALL("span")
-
+let texto = botao.querySelectorALL("span");
+if (curtiu === false) {
+    texto.textContent++;
+    curtiu = true;
+} else {
+    texto.textContent--;
+    curtiu = false;
+}
 }
 })
